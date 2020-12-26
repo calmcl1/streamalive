4c5f78aed411c6a94088dc09995fdaaaf2c4efb8
Cal McLean <calmcl1@aol.com>
2020-12-26 20:34:31 +0000

    add jawsdb url

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import Sequelize from "sequelize"
 import { Stream } from "./stream_entry"
 
 export const models = { Stream }
-export const db_url = process.env.DATABASE_URL || "mysql://root@localhost/streamalive_dev"
+export const db_url = process.env.JAWSDB_URL || process.env.DATABASE_URL || "mysql://root@localhost/streamalive_dev"
 export const seq = new Sequelize.Sequelize(db_url, {
     dialect: 'mysql',
     logging: false,
