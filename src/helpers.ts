14dcbfec2bc275977258227ed6bac2c7efc40da9
Cal McLean <calmcl1@aol.com>
2020-12-28 16:35:39 +0000

    check against heroku when checking for db sync

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { join } from 'path'
 export function shouldForceDBSync(): boolean {
     let force_sync = false
-    if (process.env.PATH?.includes("/app/.heroku") && process.env.NODE_ENV != "production") {
+    if (process.env.IS_HEROKU && process.env.NODE_ENV != "production") {
         force_sync = true
     } else {
         force_sync = process.env.NODE_ENV == "test"
